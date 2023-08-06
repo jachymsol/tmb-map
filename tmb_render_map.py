2766c47570e3ecb90f0fec2c3c09fbaecb7bea6f
jachymsol <solejabreck@gmail.com>
2023-08-06 18:40:16 +0200

    Render map html page

---
+++ tmb_render_map.py
@@ -0,0 +1,18 @@
+import folium
+from folium import plugins
+
+m = folium.Map(
+    location=[45.918291, 6.86615],
+    zoom_start=15,
+    tiles="http://api.mapy.cz/v1/maptiles/outdoor/256/{z}/{x}/{y}?apiKey=GSOZaBVvlUs9mWgGrfcxKbvc2CNT4iyU_AhLOYFLWJ8",
+    attr='<img  width="100px" src="https://api.mapy.cz/img/api/logo.svg" >'
+)
+
+with open('tmb_geojson.json', 'r') as file:
+    plugins.TimestampedGeoJson(
+        data=file,
+        period="PT1M",
+        date_options="YYYY-MM-DD HH:mm:ss"
+    ).add_to(m)
+
+m.save('tmb_map.html')
